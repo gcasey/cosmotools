27d96b424c14d7c430f4857d7f10a87b97f3718b
George Zagaris <george.zagaris@kitware.com>
2012-05-04 13:16:33 -0400

    ENH: Adding halofinder module
    Adding halofinder, currently consisting of the cosmohalo code,
    initially developed at LANL.

---
+++ halofinder/HaloFinders.h
@@ -0,0 +1,18 @@
+/**
+ * @file HaloFinders.h
+ * @brief An enumerator for all available halo-finders.
+ */
+#ifndef HALOFINDERS_H_
+#define HALOFINDERS_H_
+
+namespace hacctools {
+
+enum HaloFinders {
+  COSMO = 0,
+
+  NUMBER_OF_HALO_FINDERS
+};
+
+} /* end namespace hacctools */
+
+#endif /* HALOFINDERS_H_ */
