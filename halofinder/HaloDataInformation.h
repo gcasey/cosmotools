571fe9c800f2a7207f831bf136321bd437675452
George Zagaris <george.zagaris@kitware.com>
2012-06-11 16:29:32 -0400

    ENH: Additional halo information
    Add additional halo information to keep for each halo at every timestep.

--- halofinder/HaloDataInformation.h
+++ halofinder/HaloDataInformation.h
@@ -21,8 +21,27 @@ public:
   HaloDataInformation();
   virtual ~HaloDataInformation();
 
+  /**
+   * @brief Allocates internal data-structures
+   * @param N the number of halos
+   */
+  void Allocate(int N)
+    {
+    this->NumberOfHalos = N;
+    this->HaloCenter.resize( 3*N, 0.0 );
+    this->HaloAverageVelocity.resize( 3*N, 0.0 );
+    this->HaloVelocityDispersion.resize( N, 0.0 );
+    this->HaloMass.resize( N, 0.0 );
+    }
+
   REAL RedShift;
   int TimeStep;
+  int NumberOfHalos;
+
+  std::vector< REAL > HaloCenter;
+  std::vector< REAL > HaloVelocityDispersion;
+  std::vector< REAL > HaloAverageVelocity;
+  std::vector< REAL > HaloMass;
   std::vector< INTEGER > GlobalIds;
   std::vector< INTEGER > HaloTags;
 
