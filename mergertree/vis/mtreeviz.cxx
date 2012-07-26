c3c4abe52720b028bba7f3f1bd0367b675ca1a2a
George Zagaris <george.zagaris@kitware.com>
2012-07-26 15:03:09 -0400

    ENH: Use original halo ID and write mesh
    This commit consists of updates that use the update output which
    consists of the original halo ID. Further, the graph is also
    writen out as an unstructured mesh that can be visualized in PV.

--- mergertree/vis/mtreeviz.cxx
+++ mergertree/vis/mtreeviz.cxx
@@ -26,21 +26,32 @@
 
 // VTK includes
 #include "vtkActor.h"
+#include "vtkCellArray.h"
+#include "vtkCellData.h"
 #include "vtkDataSetAttributes.h"
 #include "vtkDoubleArray.h"
 #include "vtkGraphLayout.h"
 #include "vtkGraphLayoutView.h"
 #include "vtkGraphToPolyData.h"
+#include "vtkIdList.h"
 #include "vtkMutableDirectedGraph.h"
+#include "vtkPointData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkPolyDataWriter.h"
+#include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
 #include "vtkRenderer.h"
 #include "vtkSmartPointer.h"
+#include "vtkTree.h"
 #include "vtkTreeLayoutStrategy.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkUnstructuredGridWriter.h"
 
 struct TreeNodeInfo {
 
   // Data read from file
   float HaloNumber;
+  float OriginalHaloIdx;
   float DescendantNumber;
   float TimeStep;
   float Volume;
@@ -57,19 +68,17 @@ struct TreeNodeInfo {
    */
   void GetGraphNodePosition(double pos[3])
     {
-    pos[0] = static_cast<double>(HaloNumber);
-    pos[1] = this->TimeStep;
+    pos[0] = static_cast<double>(this->OriginalHaloIdx);
+    pos[1] = static_cast<double>(this->TimeStep);
     pos[2] = 0.0;
     }
 
   /**
    * @return HashCode for
    */
- std::string GetHashCode()
+ int GetHashCode()
    {
-   std::ostringstream oss;
-   oss << this->HaloNumber << "." << this->TimeStep;
-   return( oss.str() );
+   return( static_cast<int>(this->HaloNumber) );
    }
 
  /**
@@ -79,17 +88,18 @@ struct TreeNodeInfo {
  void Println()
    {
    std::cout << "=======\n";
-   std::cout << "HALO ID: "    << this->HaloNumber       << std::endl;
-   std::cout << "DESCENDANT: " << this->DescendantNumber << std::endl;
-   std::cout << "Time-step: "  << this->TimeStep         << std::endl;
-   std::cout << "Volume: "     << this->Volume           << std::endl;
-   std::cout << "PosX: "       << this->PosX             << std::endl;
-   std::cout << "PosY: "       << this->PosY             << std::endl;
-   std::cout << "PosZ: "       << this->PosZ             << std::endl;
-   std::cout << "VelX: "       << this->VelX             << std::endl;
-   std::cout << "VelY: "       << this->VelY             << std::endl;
-   std::cout << "VelZ: "       << this->VelZ             << std::endl;
-   std::cout << "hashcode: "   << this->GetHashCode()    << std::endl;
+   std::cout << "HALO ID: "         << this->HaloNumber       << std::endl;
+   std::cout << "ORIGINAL HALO ID:" << this->OriginalHaloIdx << std::endl;
+   std::cout << "DESCENDANT: "      << this->DescendantNumber << std::endl;
+   std::cout << "Time-step: "       << this->TimeStep         << std::endl;
+   std::cout << "Volume: "          << this->Volume           << std::endl;
+   std::cout << "PosX: "            << this->PosX             << std::endl;
+   std::cout << "PosY: "            << this->PosY             << std::endl;
+   std::cout << "PosZ: "            << this->PosZ             << std::endl;
+   std::cout << "VelX: "            << this->VelX             << std::endl;
+   std::cout << "VelY: "            << this->VelY             << std::endl;
+   std::cout << "VelZ: "            << this->VelZ             << std::endl;
+   std::cout << "hashcode: "        << this->GetHashCode()    << std::endl;
    std::cout.flush();
    }
 
@@ -98,11 +108,11 @@ struct TreeNodeInfo {
 // Stores all the nodes in the graph
 std::vector< TreeNodeInfo > GraphNodes;
 
-// Mapping of tree nodes to graph
-std::map< std::string, int > TreeNode2Graph;
+vtkUnstructuredGrid *graphGrid;
+
 
 /**
- * @brief
+ * @brief Reads data into
  * @param fileName
  * @return
  */
@@ -120,6 +130,7 @@ bool ReadData( std::string fileName )
   while( !ifs.eof() )
     {
     ifs.read((char*)(&tnode.HaloNumber),sizeof(float));
+    ifs.read((char*)(&tnode.OriginalHaloIdx),sizeof(float));
     ifs.read((char*)(&tnode.DescendantNumber),sizeof(float));
     ifs.read((char*)(&tnode.TimeStep),sizeof(float));
     ifs.read((char*)(&tnode.Volume),sizeof(float));
@@ -138,14 +149,6 @@ bool ReadData( std::string fileName )
     tnode.Println();
     GraphNodes.push_back( tnode );
 
-    std::string hashCode = tnode.GetHashCode();
-    if( TreeNode2Graph.find(hashCode) != TreeNode2Graph.end() )
-      {
-      std::cerr << "ERROR: Duplicate Entries in TreeNode2Graph\n";
-      return false;
-      }
-
-    TreeNode2Graph[ hashCode ] = GraphNodes.size()-1;
     } // END while !ifs.eof()
 
   std::cout << "Number of nodes: " << GraphNodes.size() << std::endl;
@@ -163,12 +166,25 @@ void BuildGraph(vtkMutableDirectedGraph *g)
 {
   assert("pre: graph should not be NULL!" && (g != NULL) );
 
+  graphGrid = vtkUnstructuredGrid::New();
+  vtkCellArray *meshLines = vtkCellArray::New();
+  meshLines->Allocate(meshLines->EstimateSize(GraphNodes.size()-1,2));
+
+
   vtkDoubleArray *volumes = vtkDoubleArray::New();
   volumes->SetName("HaloDensity");
   volumes->SetNumberOfComponents( 1 );
   volumes->SetNumberOfTuples( GraphNodes.size() );
 
-  double *vptr = static_cast<double*>(volumes->GetPointer(0));
+  vtkDoubleArray *velocities = vtkDoubleArray::New();
+  velocities->SetName("Velocities");
+  velocities->SetNumberOfComponents( 3 );
+  velocities->SetNumberOfTuples( GraphNodes.size() );
+
+  vtkPoints *points = vtkPoints::New();
+
+  double *velptr = static_cast<double*>(velocities->GetPointer(0));
+  double *vptr   = static_cast<double*>(volumes->GetPointer(0));
 
   // STEP 0: Insert nodes
   double pos[3];
@@ -177,20 +193,43 @@ void BuildGraph(vtkMutableDirectedGraph *g)
     GraphNodes[i].GetGraphNodePosition(pos);
     vptr[ i ] = GraphNodes[ i ].Volume;
 
+    velptr[ i*3   ] = GraphNodes[ i ].VelX;
+    velptr[ i*3+1 ] = GraphNodes[ i ].VelY;
+    velptr[ i*3+2 ] = GraphNodes[ i ].VelZ;
+
     vtkIdType idx = g->AddVertex();
-//    g->GetPoints()->SetPoint( idx, pos);
+    points->InsertNextPoint( pos );
     } // END for all graph nodes
+  g->SetPoints( points );
+  graphGrid->SetPoints( points );
+  points->Delete();
+
   g->GetVertexData()->AddArray( volumes );
+  graphGrid->GetPointData()->AddArray( volumes );
   volumes->Delete();
 
+  g->GetVertexData()->AddArray( velocities );
+  graphGrid->GetPointData()->AddArray( velocities );
+  velocities->Delete();
+
+
   // STEP 1: Create edges
+  vtkIdList *edgeNodes = vtkIdList::New();
+  edgeNodes->SetNumberOfIds(2);
   for( int i=0; i < GraphNodes.size(); ++i )
     {
     if( static_cast<int>(GraphNodes[i].DescendantNumber) != -1)
       {
       g->AddEdge( i, GraphNodes[i].DescendantNumber);
+      edgeNodes->SetId(0,i);
+      edgeNodes->SetId(1,GraphNodes[i].DescendantNumber);
+      meshLines->InsertNextCell( edgeNodes );
       }
     } // END for all graph nodes
+  edgeNodes->Delete();
+
+  graphGrid->SetCells(VTK_LINE,meshLines);
+  meshLines->Delete();
 }
 
 /**
@@ -221,19 +260,18 @@ int main(int argc, char **argv)
    vtkSmartPointer<vtkMutableDirectedGraph>::New();
   BuildGraph( g );
 
-  // STEP 2: Visualize the graph
+  // STEP 3: Write poly data
+  vtkUnstructuredGridWriter *writer = vtkUnstructuredGridWriter::New();
+  writer->SetFileName( "ugrid.vtk" );
+  writer->SetInputData( graphGrid );
+  writer->Write();
+  writer->Delete();
+
+  // STEP 3: Visualize the graph
   vtkSmartPointer<vtkGraphLayoutView> graphLayoutView =
     vtkSmartPointer<vtkGraphLayoutView>::New();
 
-  // TODO: Ask Jeff about how to use the Tree Layout strategy -- The problem
-  // is that Boost is required but, VTK_USE_BOOST is not available as an option
-  // that can be turned on. When manually getting rid of the #ifdef VTK does
-  // not compile.
-  //
-//  graphLayoutView->SetLayoutStrategyToCosmicTree();
-  //
-
-
+  graphLayoutView->SetVertexScalarBarVisibility(true);
   graphLayoutView->AddRepresentationFromInput(g);
   graphLayoutView->SetColorVertices(true);
   graphLayoutView->SetVertexColorArrayName("HaloDensity");
@@ -241,5 +279,6 @@ int main(int argc, char **argv)
   graphLayoutView->Render();
   graphLayoutView->GetInteractor()->Start();
 
+  graphGrid->Delete();
   return EXIT_SUCCESS;
 }
