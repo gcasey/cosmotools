832c676d30993a3778fff9e35bcf8f0daf92275d
George Zagaris <george.zagaris@kitware.com>
2012-08-06 17:41:41 -0400

    DOC: Correct some documentation

--- sfprobe/StructureFormationProbe.h
+++ sfprobe/StructureFormationProbe.h
@@ -119,8 +119,8 @@ protected:
   bool MapTetToEulerSpace(INTEGER tet[4], REAL nodes[12]);
 
   /**
-   * @brief Checks if the node is within an interior box constructed based on the
-   * langrangian grid box extent domain.
+   * @brief Checks if the node is within an interior box constructed based on
+   * the langrangian grid box extent domain.
    * @param nodeIdx
    * @return
    */
