764dfdf5f7a7110eb882b302a0496948f6e30b40
George Zagaris <george.zagaris@kitware.com>
2012-09-04 16:48:14 -0400

    ENH: LANLHalofinder integration w/insitu framework
    Initial wrapper of LANL halo-finder for integration within the
    in situ analysis framework.

---
+++ framework/LANLHaloFinderAnalysisTool.cxx
@@ -0,0 +1,135 @@
+#include "LANLHaloFinderAnalysisTool.h"
+
+#include <cassert>
+
+#include <mpi.h>
+
+namespace cosmotk
+{
+
+LANLHaloFinderAnalysisTool::LANLHaloFinderAnalysisTool()
+{
+  this->Name               = "LANLHaloFinder";
+  this->LinkingLength      = 0.2;
+  this->PMIN               = 250;
+  this->CenterFinderMethod = AVERAGE;
+  this->ComputSODHalos     = false;
+  this->RHO_C              = 2.77537e+11;
+  this->INITIAL_SOD_MASS   = 1e+14;
+  this->MIN_RADIUS_FACTOR  = 0.5;
+  this->MAX_RADIUS_FACTOR  = 2;
+  this->NUMBER_OF_BINS     = 20;
+  this->FOF_SIZE_THRESHOLD = 500;
+  this->Communicator       = MPI_COMM_NULL;
+
+  this->HaloFinder = new cosmologytools::CosmoHaloFinderP();
+}
+
+//-----------------------------------------------------------------------------
+LANLHaloFinderAnalysisTool::~LANLHaloFinderAnalysisTool()
+{
+  if(this->HaloFinder != NULL)
+    {
+    delete this->HaloFinder;
+    this->HaloFinder = NULL;
+    }
+}
+
+//-----------------------------------------------------------------------------
+void LANLHaloFinderAnalysisTool::ParseParameters()
+{
+  // STEP 0: Parse basic parameters, as defined by super-class
+  this->ParseBasicParameters();
+
+  // STEP 1: Parse LANL halo-finder parameters
+  this->LinkingLength = static_cast<REAL>(this->GetDoubleParameter("BB"));
+  this->PMIN = this->GetIntParameter("PMIN");
+
+  this->CenterFinderMethod = this->GetIntParameter("CENTER_FINDER_METHOD");
+  assert("pre: Invalid center finder method" &&
+          (this->CenterFinderMethod >= 0) &&
+          (this->CenterFinderMethod < NUMBER_OF_CENTER_FINDER_METHODS));
+
+  this->ComputSODHalos = this->GetBooleanParameter("COMPUTE_SOD_HALOS");
+
+  if( this->ComputSODHalos )
+    {
+    this->RHO_C = static_cast<REAL>(this->GetDoubleParameter("RHO_C"));
+    this->INITIAL_SOD_MASS =
+        static_cast<REAL>(this->GetDoubleParameter("INITIAL_SOD_MASS"));
+    this->MIN_RADIUS_FACTOR =
+        static_cast<REAL>(this->GetDoubleParameter("MIN_RADIUS_FACTOR"));
+    this->MAX_RADIUS_FACTOR =
+        static_cast<REAL>(this->GetDoubleParameter("MAX_RADIUS_FACTOR"));
+    this->NUMBER_OF_BINS = this->GetIntParameter("NUMBER_OF_BINS");
+    this->FOF_SIZE_THRESHOLD = this->GetIntParameter("FOF_SIZE_THRESHOLD");
+    }
+}
+
+//-----------------------------------------------------------------------------
+void LANLHaloFinderAnalysisTool::Execute(SimulationParticles *particles)
+{
+  assert("pre: halofinder is NULL!" && (this->HaloFinder != NULL));
+  assert("pre: input particles are NULL!" && (particles != NULL) );
+  assert("pre: MPI communicator is NULL!" &&
+         (this->Communicator != MPI_COMM_NULL) );
+
+  // STEP 0: short-circuit here in the unlike event that we don't have any
+  // particles at this time-step
+  if( particles->NumParticles == 0 )
+    {
+    return;
+    }
+
+  // STEP 1: parse the analysis tool parameters
+  this->ParseParameters();
+
+  // STEP 2: Setup halo-finder
+  if( this->GenerateOutput )
+    {
+    this->HaloFinder->setParameters(
+        "",this->BoxLength,this->NG,this->NDIM,
+        this->PMIN, this->LinkingLength);
+    }
+  else
+    {
+    this->HaloFinder->setParameters(
+        this->OutputFile,this->BoxLength,this->NG,this->NDIM,
+        this->PMIN, this->LinkingLength);
+    }
+
+  // STEP 3: Register the particles with the halo-finder
+  // NOTE: cast this to long here since the halo-finder stores the total
+  // number of particles in an ivar that is a long.
+  long numParticles = static_cast<long>(particles->NumParticles);
+  this->HaloFinder->setParticles(
+      particles->X,particles->Y,particles->Z,
+      particles->VX,particles->VY,particles->VZ,
+      particles->Potential,particles->GlobalIds,
+      particles->Mask,particles->State,
+      numParticles);
+
+  // STEP 4: Run the halo-finder at each rank
+  this->HaloFinder->executeHaloFinder();
+
+  // STEP 5: Merge results across ranks
+  this->HaloFinder->collectHalos(true /*clearSerial*/);
+  this->HaloFinder->mergeHalos();
+}
+
+//-----------------------------------------------------------------------------
+void LANLHaloFinderAnalysisTool::WriteOutput()
+{
+  assert("pre: halofinder is NULL!" && (this->HaloFinder != NULL));
+  assert("pre: MPI communicator is NULL!" &&
+         (this->Communicator != MPI_COMM_NULL) );
+
+  if( !this->GenerateOutput )
+    {
+    return;
+    }
+
+  this->HaloFinder->writeTaggedParticles(true /*clearSerial*/);
+}
+
+} /* namespace cosmotk */
---
+++ framework/LANLHaloFinderAnalysisTool.h
@@ -0,0 +1,77 @@
+/**
+ * @brief
+ */
+#ifndef LANLHALOFINDERANALYSISTOOL_H_
+#define LANLHALOFINDERANALYSISTOOL_H_
+
+#include "CosmologyToolsMacros.h"
+#include "AnalysisTool.h"
+#include "CosmoHaloFinderP.h"
+
+namespace cosmotk
+{
+
+enum CenterFinder
+{
+  AVERAGE = 0,
+  CENTER_OF_MASS = 1,
+  MBP = 2,
+  MCP = 3,
+
+  NUMBER_OF_CENTER_FINDER_METHODS
+};
+
+class LANLHaloFinderAnalysisTool : public AnalysisTool
+{
+public:
+
+  /**
+   * @brief Default constructor
+   */
+  LANLHaloFinderAnalysisTool();
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~LANLHaloFinderAnalysisTool();
+
+  /**
+   * @brief Parses the parameters of this analysis tool
+   */
+  virtual void ParseParameters();
+
+  /**
+   * @brief Executes the halo-finder on the given particle input
+   * @param particles pointer to the simulation particles
+   * @pre particles != NULL
+   * @pre this->Communicator != MPI_COMM_NULL
+   */
+  virtual void Execute(SimulationParticles *particles);
+
+  /**
+   * @brief Writes the output of the halo-finder
+   * @see AnalysisTool::WriteOutput
+   */
+  virtual void WriteOutput();
+
+protected:
+
+  // LANLHaloFinder parameters from the configuration file
+  REAL LinkingLength;
+  INTEGER PMIN;
+  INTEGER CenterFinderMethod;
+  bool ComputSODHalos;
+  REAL RHO_C;
+  REAL INITIAL_SOD_MASS;
+  REAL MIN_RADIUS_FACTOR;
+  REAL MAX_RADIUS_FACTOR;
+  INTEGER NUMBER_OF_BINS;
+  INTEGER FOF_SIZE_THRESHOLD;
+
+  cosmologytools::CosmoHaloFinderP *HaloFinder;
+private:
+  DISABLE_COPY_AND_ASSIGNMENT(LANLHaloFinderAnalysisTool);
+};
+
+} /* namespace cosmotk */
+#endif /* LANLHALOFINDERANALYSISTOOL_H_ */
