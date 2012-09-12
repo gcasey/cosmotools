e33ca6bcb49bea7df870a3294f4f5caf766e6130
George Zagaris <george.zagaris@kitware.com>
2012-09-12 12:35:31 -0400

    ENH: Macro to populate global package source list
    A simple macro used to populate the global package source list
    for each package.

---
+++ cmake/GetPackageSources.cmake
@@ -0,0 +1,19 @@
+# - Given a list of sources with relative path, get the list with absolute path
+# GetPackageSources(pkg basepath srclist)
+# - macro which takes a source list and converts it to
+#
+# The following variables are expected to have been set:
+#
+#   PACKAGE_${pkg}_SOURCES
+
+
+
+macro(GetPackageSources pkg basepath srclist)
+    message(STATUS "Setting package sources for ${pkg}")
+    foreach(src ${srclist})
+      set(PACKAGE_${pkg}_SOURCES
+        ${PACKAGE_${pkg}_SOURCES}
+        ${basepath}/${src}
+        CACHE INTENRNAL "${pkg} sources")
+    endforeach()
+endmacro(GetPackageSources)
