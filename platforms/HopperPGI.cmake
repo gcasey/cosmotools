8b506cb2c3ff25f3bbefc4e8f4be5458577f7ce1
George Zagaris <george.zagaris@kitware.com>
2012-09-12 15:39:55 -0400

    ENH: Cross-compiler on Hopper using PGI
    Initial cross-compilation code for hopper
    using PGI compilers

---
+++ platforms/HopperPGI.cmake
@@ -0,0 +1,15 @@
+## set the name of the system
+set(CMAKE_SYSTEM_NAME hopper-pgi)
+
+## setup compilers
+set(BASE_COMPILER_DIR /opt/cray/xt-asyncpe/5.12/bin)
+set(CMAKE_C_COMPILER ${BASE_COMPILER_DIR}/cc)
+set(CMAKE_CXX_COMPILER ${BASE_COMPILER_DIR}/CC)
+set(CMAKE_FORTRAN_COMPILER ${BASE_COMPILER_DIR}/ftn)
+
+set(MPI_C_COMPILER ${CMAKE_C_COMPILER})
+set(MPI_CXX_COMPILER ${CMAKE_CXX_COMPILER})
+set(MPI_Fortran_COMPILER ${CMAKE_FORTRAN_COMPILER})
+
+## set behavior for finding programs
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
