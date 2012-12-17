4c36fb6ec87ed5ad46553693a08bd716c297507b
George Zagaris <george.zagaris@kitware.com>
2012-12-17 18:34:42 -0500

    ENH: Encapsulated merger-tree in a single function
    Encapsulated merger-tree logic within a signle function.

--- halofinder/HaloMergerTreeKernel.cxx
+++ halofinder/HaloMergerTreeKernel.cxx
@@ -1,4 +1,6 @@
 #include "HaloMergerTreeKernel.h"
+
+#include "DistributedHaloEvolutionTree.h"
 #include "Halo.h"
 
 #include <cassert>
@@ -8,6 +10,10 @@ namespace cosmotk
 
 HaloMergerTreeKernel::HaloMergerTreeKernel()
 {
+  this->Timesteps[0] = this->Timesteps[1] = 0;
+  this->Sizes[0]     = this->Sizes[1]     = 1;
+  this->Halos1 = NULL;
+  this->Halos2 = NULL;
   this->MergerTreeThreshold = 10;
 }
 
@@ -19,30 +25,89 @@ HaloMergerTreeKernel::~HaloMergerTreeKernel()
 
 
 //------------------------------------------------------------------------------
-void HaloMergerTreeKernel::ComputeMergerTree(
-    const int t1, std::vector< Halo > &haloSet1,
-    const int t2, std::vector< Halo > &haloSet2 )
+void HaloMergerTreeKernel::UpdateMergerTree(
+      const int t1, Halo **haloSet1, const int M,
+      const int t2, Halo **haloSet2, const int N,
+      DistributedHaloEvolutionTree *t )
 {
-  assert("pre: t2 > t1 must hold true" && (t2 > t1) );
+  this->RegisterHalos( t1,haloSet1,M, t2,haloSet2,N );
+  this->ComputeMergerTree();
+  this->UpdateHaloEvolutionTree( t );
+}
 
-  int nrows = static_cast<int>( haloSet1.size() );
-  int ncol  = static_cast<int>( haloSet2.size() );
+//------------------------------------------------------------------------------
+void HaloMergerTreeKernel::RegisterHalos(
+    const int t1, Halo **haloSet1, const int M,
+    const int t2, Halo **haloSet2, const int N)
+{
+  assert("pre: t1 < t2" && (t1 < t2) );
+  assert("pre: halo set 1 should not be empty" && (M >= 1) );
+  assert("pre: halo set 2 should not be empty" && (N >= 1) );
+  assert("pre: null haloset!" && (haloSet1 != NULL) );
+  assert("pre: null haloset!" && (haloSet2 != NULL) );
+
+  this->Timesteps[0] = t1;
+  this->Timesteps[1] = t2;
+  this->Sizes[0] = M;
+  this->Sizes[1] = N;
+  this->Halos1 = haloSet1;
+  this->Halos2 = haloSet2;
+}
+
+//------------------------------------------------------------------------------
+void HaloMergerTreeKernel::ComputeMergerTree( )
+{
+  assert("pre: halos != NULL" && (this->Halos1 != NULL) );
+  assert("pre: halos != NULL" && (this->Halos2 != NULL) );
+
+  int nrows = this->Sizes[0];
+  int ncol  = this->Sizes[1];
   this->HaloSimilarityMatrix.resize( nrows*ncol );
 
   for( int row=0; row < nrows; ++row )
     {
     for( int col=0; col < ncol; ++col )
       {
-      int overlap = haloSet1[ row ].Intersect( &haloSet2[col ] );
+      int overlap = this->Halos1[row]->Intersect(this->Halos2[col]);
       this->HaloSimilarityMatrix[row*ncol+col] = overlap;
       } // END for all columns
     } // END for all rows
 }
 
 //------------------------------------------------------------------------------
-void HaloMergerTreeKernel::UpdateHaloEvolutionTree()
+void HaloMergerTreeKernel::UpdateHaloEvolutionTree(
+        DistributedHaloEvolutionTree *t)
 {
-  // TODO: implement this
+  assert("pre: halo evolution tree should not be NULL" && (t!=NULL) );
+
+  // STEP 0: Append nodes to the tree
+  if( t->IsEmpty() )
+    {
+    t->AppendNodes(this->Halos1,this->Sizes[0]);
+    t->AppendNodes(this->Halos2,this->Sizes[1]);
+    }
+  else
+    {
+    t->AppendNodes(this->Halos2,this->Sizes[1]);
+    }
+
+  // STEP 1: Create Edges
+  int nrows = this->Sizes[0];
+  int ncol  = this->Sizes[1];
+  for(int row=0; row < nrows; ++row)
+    {
+    for(int col=0; col < ncol; ++col)
+      {
+      int overlap = this->HaloSimilarityMatrix[row*ncol+col];
+      if(overlap > this->MergerTreeThreshold)
+        {
+        t->CreateEdge(
+            this->Halos1[row]->GetHashCode(),
+            this->Halos2[col]->GetHashCode(),
+            overlap);
+        } // if the halos are similar
+      } // END for all columns
+    } // END for all rows
 
 }
 
--- halofinder/HaloMergerTreeKernel.h
+++ halofinder/HaloMergerTreeKernel.h
@@ -1,5 +1,7 @@
 /**
- * @brief
+ * @brief Merger-tree kernel that supports incremental building of a
+ * halo-evolution tree instance, used to capture the halo formation
+ * history during the course of a simulation.
  */
 #ifndef HALOMERGERTREEKERNEL_H_
 #define HALOMERGERTREEKERNEL_H_
@@ -14,6 +16,7 @@ namespace cosmotk
 
 // Forward declarations
 class Halo;
+class DistributedHaloEvolutionTree;
 
 class HaloMergerTreeKernel
 {
@@ -22,25 +25,67 @@ public:
   virtual ~HaloMergerTreeKernel();
 
   /**
-   * @brief Given two sets of halos at different timesteps, this method
-   * will compute the corresponding merger-tree.
-   * @param t1 the time-step for the 1st halo set.
-   * @param haloSet1 the 1st halo set.
-   * @param t2 the time-step for the 2nd halo set.
-   * @param haloSet2 the 2nd halo set.
+   * @brief Given two sets of halos at different time-steps, this method
+   * updates the merger-tree, represented by a user-supplied halo evolution
+   * tree instance.
+   * @param t1 the time-step of the first set of halos (in).
+   * @param haloSet1 array of halos at t1 (in).
+   * @param M the number of halos at t1 (in).
+   * @param t2 the time-step of the 2nd set of halos (in).
+   * @param haloSet2 array of halos at t2 (in).
+   * @param N the number of halos at t2 (in).
+   * @param t the halo evolution tree (out).
+   * @pre (t1 < t2).
+   * @pre (M >= 1) && (N >= 1).
+   * @pre (haloSet1 != NULL) && (haloSet2 != NULL).
+   * @pre (t != NULL).
    */
-  void ComputeMergerTree(
-      const int t1, std::vector< Halo > &haloSet1,
-      const int t2, std::vector< Halo > &haloSet2);
+  virtual void UpdateMergerTree(
+        const int t1, Halo **haloSet1, const int M,
+        const int t2, Halo **haloSet2, const int N,
+        DistributedHaloEvolutionTree *t);
+
+protected:
+
+  int Timesteps[2]; // The two time-steps of halo-information.
+  int Sizes[2];     // The number of halos for each of the two time-steps.
+  Halo **Halos1;     // The set of halos at time-step t1.
+  Halo **Halos2;     // The set of halos at time-step t2.
+
+  std::vector< int > HaloSimilarityMatrix; // Flat 2-D matrix
+  int MergerTreeThreshold;
+
+
+  /**
+   * @brief Registers the halos at the given timesteps.
+   * @param t1 the time-step of the first halo set.
+   * @param haloSet1 array of halos at t1.
+   * @param M the number of halos in haloSet1.
+   * @param t2 the time-step of the second halo set.
+   * @param haloSet2 array of halos at t2.
+   * @param N the number of halos in haloSet2.
+   * @pre (t1 < t2).
+   * @pre (M >= 1) && (N >= 1).
+   * @pre (haloSet1 != NULL) && (haloSet2 != NULL).
+   */
+  void RegisterHalos(
+       const int t1, Halo **haloSet1, const int M,
+       const int t2, Halo **haloSet2, const int N);
+
+  /**
+   * @brief Given two sets of halos at different time-steps, this method
+   * will compute the corresponding merger-tree. The merger-tree is defined
+   * in an MxN matrix, H, wherein H(i,j) > 0 indicates that halo_i \in t_1 is
+   * a parent of halo_j \in t_2.
+   */
+  void ComputeMergerTree();
 
   /**
    * @brief Updates the halo-evolution tree, from the current similarity matrix.
+   * @param t the halo evolution tree instance to update.
    */
-  void UpdateHaloEvolutionTree();
-
-protected:
-  std::vector< int > HaloSimilarityMatrix; // Flat 2-D matrix
-  int MergerTreeThreshold;
+  void UpdateHaloEvolutionTree(
+          DistributedHaloEvolutionTree *t);
 
 private:
   DISABLE_COPY_AND_ASSIGNMENT(HaloMergerTreeKernel);
