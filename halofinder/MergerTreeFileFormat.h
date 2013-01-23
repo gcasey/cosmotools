86b0ca2500dfe14aa94fe78facdbad0408959542
George Zagaris <george.zagaris@kitware.com>
2013-01-23 07:43:22 -0500

    ENH: Add MergerTreeFileFormat enum

---
+++ halofinder/MergerTreeFileFormat.h
@@ -0,0 +1,21 @@
+#ifndef MERGERTREEFILEFORMAT_H_
+#define MERGERTREEFILEFORMAT_H_
+
+
+namespace cosmotk {
+
+class MergerTreeFileFormat
+{
+public:
+  enum
+    {
+    DIY = 0,
+    GENERIC_IO,
+
+    UNDEFINED
+    };
+};
+
+}
+
+#endif /* MERGERTREEFILEFORMAT_H_ */
