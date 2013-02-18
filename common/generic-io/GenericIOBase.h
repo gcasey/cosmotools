6abaa02d979aa7c52e7dc5f5a002424232bc7869
George Zagaris <george.zagaris@kitware.com>
2013-02-18 17:12:26 -0500

    ENH: Add header data-structures in GenericIOBase
    Added GlobalHeader,RankHeader and VariableHeader in the
    GenericIOBase header since these data-structures are used
    by both readers/writers.

--- common/generic-io/GenericIOBase.h
+++ common/generic-io/GenericIOBase.h
@@ -14,6 +14,39 @@
 namespace cosmotk
 {
 
+//------------------------------------------------------------------------------
+//                INTERNAL DATASTRUCTURES
+//------------------------------------------------------------------------------
+static const size_t NameSize = 256;
+
+struct VariableHeader {
+  char Name[NameSize];
+  uint64_t Flags;
+  uint64_t Size;
+} __attribute__((packed));
+
+struct RankHeader {
+  uint64_t Coords[3];
+  uint64_t NElems;
+  uint64_t Start;
+  uint64_t GlobalRank;
+} __attribute__((packed));
+
+struct GlobalHeader {
+  char Magic[MagicSize];
+  uint64_t HeaderSize;
+  uint64_t NElems; // The global total
+  uint64_t Dims[3];
+  uint64_t NVars;
+  uint64_t VarsSize;
+  uint64_t VarsStart;
+  uint64_t NRanks;
+  uint64_t RanksSize;
+  uint64_t RanksStart;
+  uint64_t GlobalHeaderSize;
+  double   PhysOrigin[3];
+  double   PhysScale[3];
+} __attribute__((packed));
 
 struct VariableInfo
 {
@@ -30,7 +63,7 @@ struct VariableInfo
   bool IsPhysCoordX, IsPhysCoordY, IsPhysCoordZ;
   bool MaybePhysGhost;
 };
-
+//------------------------------------------------------------------------------
 
 class GenericIOBase
 {
