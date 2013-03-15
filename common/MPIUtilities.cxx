74a74ce9ec1f85d92e27de7fc29ab2de9370b33d
George Zagaris <george.zagaris@kitware.com>
2013-03-15 17:33:26 -0400

    ENH:Implement MPIUtilities
    Implement MPIUtilities singleton class that provides functionality
    for printing to stdout, either on the master node, i.e., rank 0, or
    in a synchronized manner, that is, rank i, will print before rank i+1.
    This class also serves as the place holder for other similar functions
    to be added in the future.

---
+++ common/MPIUtilities.cxx
@@ -0,0 +1,105 @@
+#include "MPIUtilities.h"
+
+#include <cstdio>
+#include <cstdarg>
+
+namespace cosmotk
+{
+
+MPIUtilities::MPIUtilities()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+//------------------------------------------------------------------------------
+MPIUtilities::~MPIUtilities()
+{
+  // TODO Auto-generated destructor stub
+}
+
+//------------------------------------------------------------------------------
+void MPIUtilities::Printf(MPI_Comm comm, char *fmt,...)
+{
+  // Sanity check
+  assert("pre: MPI communicator should not be NULL!" && (comm != MPI_COMM_NULL) );
+  assert("pre: NULL formatted message!" && (fmt != NULL) );
+
+  int rank = -1;
+  MPI_Comm_rank(comm,&rank);
+  if( rank==0 )
+    {
+    va_list argptr;
+    va_start(argptr,fmt);
+    vprintf(fmt,argptr);
+    fflush(stdout);
+    va_end(argptr);
+    }
+  MPI_Barrier(comm);
+}
+
+//------------------------------------------------------------------------------
+void MPIUtilities::SynchronizedPrintf(MPI_Comm comm,char *fmt,...)
+{
+  // Sanity check
+  assert("pre: MPI communicator should not be NULL!" && (comm != MPI_COMM_NULL) );
+  assert("pre: NULL formatted message!" && (fmt != NULL) );
+
+  int rank     = -1;
+  int numRanks = -1;
+  MPI_Comm_rank(comm,&rank);
+  MPI_Comm_size(comm,&numRanks);
+  MPI_Request nullRequest = MPI_REQUEST_NULL;
+
+  if( rank == 0 )
+    {
+    // This is the first rank
+
+    // STEP 0: Print message
+    va_list argptr;
+    va_start(argptr,fmt);
+    printf("[%d]: ",rank);
+    vprintf(fmt,argptr);
+    fflush(stdout);
+    va_end(argptr);
+
+    // STEP 1: Tell next process to print, if any
+    if( numRanks > 1)
+      {
+      MPI_Isend(NULL,0,MPI_INT,rank+1,MPI_ANY_TAG,comm,&nullRequest);
+      }
+    }
+  else if( rank == numRanks-1 )
+    {
+    // Last rank
+
+    // STEP 0: Block until previous process completes
+    MPI_Recv(NULL,0,MPI_INT,rank-1,MPI_ANY_TAG,comm,MPI_STATUS_IGNORE);
+
+    // STEP 1: Print message
+    va_list argptr;
+    va_start(argptr,fmt);
+    printf("[%d]: ",rank);
+    vprintf(fmt,argptr);
+    fflush(stdout);
+    va_end(argptr);
+    }
+  else
+    {
+    // STEP 0: Block until previous process completes
+    MPI_Recv(NULL,0,MPI_INT,rank-1,MPI_ANY_TAG,comm,MPI_STATUS_IGNORE);
+
+    // STEP 1: Print message
+    va_list argptr;
+    va_start(argptr,fmt);
+    printf("[%d]: ",rank);
+    vprintf(fmt,argptr);
+    fflush(stdout);
+    va_end(argptr);
+
+    // STEP 2: tell next process to print
+    MPI_Isend(NULL,0,MPI_INT,rank+1,MPI_ANY_TAG,comm,&nullRequest);
+    }
+}
+
+} /* namespace cosmotk */
---
+++ common/MPIUtilities.h
@@ -0,0 +1,48 @@
+/**
+ * @brief A singleton class that implements generic functions for MPI apps/libs.
+ */
+#ifndef MPIUTILITIES_H_
+#define MPIUTILITIES_H_
+
+#include "CosmologyToolsMacros.h"
+
+#include <mpi.h>
+
+namespace cosmotk
+{
+
+class MPIUtilities
+{
+public:
+
+  /**
+   * @brief Rank 0 prints the formatted message to stdout
+   * @param comm the communicator used by the parallel application
+   * @param fmt the formatted message to print
+   * @note This is a collective call, all processes must call this method.
+   * @pre comm != MPI_COMM_NULL
+   * @pre fmt != NULL
+   */
+  static void Printf(MPI_Comm comm, char *fmt,...);
+
+  /**
+   * @brief Each process prints the formatted message in stdout in rank order.
+   * That is, process "i" prints its message right after process "i-1".
+   * @param comm the communicator used by the parallel application
+   * @param fmt the formatted message to print
+   * @note This is a collective call, all processes must call this method.
+   * @pre comm != MPI_COMM_NULL
+   * @pre fmt != NULL
+   */
+  static void SynchronizedPrintf(MPI_Comm comm, char *fmt,...);
+
+protected:
+  MPIUtilities();
+  virtual ~MPIUtilities();
+
+private:
+  DISABLE_COPY_AND_ASSIGNMENT(MPIUtilities);
+};
+
+} /* namespace cosmotk */
+#endif /* MPIUTILITIES_H_ */
