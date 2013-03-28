3b49603371bccaa9222ca639f63c44da83ab98bf
George Zagaris <george.zagaris@kitware.com>
2013-03-28 09:39:35 -0700

    COMP: Hopper cross-compilation updates
    Changed hopper platform CMake to use the default GNU compiler and MPI
    library.

--- platforms/hopper-cray-gnu.cmake
+++ platforms/hopper-cray-gnu.cmake
@@ -1,7 +1,7 @@
-## Configuration on Hopper using PGI compilers (default)
+## Configuration on Hopper using GNU compiler
 
 ## setup compilers
-set(BASE_COMPILER_DIR /opt/cray/xt-asyncpe/5.12/bin)
+set(BASE_COMPILER_DIR /opt/cray/xt-asyncpe/default/bin)
 set(CMAKE_C_COMPILER ${BASE_COMPILER_DIR}/cc)
 set(CMAKE_CXX_COMPILER ${BASE_COMPILER_DIR}/CC)
 set(CMAKE_FORTRAN_COMPILER ${BASE_COMPILER_DIR}/ftn)
@@ -11,7 +11,7 @@ set(CMAKE_CXX_FLAGS "${GNUFLAGS}")
 set(CMAKE_C_FLAGS "${GNUFLAGS}")
 
 set(CMAKE_FIND_ROOT_PATH
-    /opt/cray/mpt/5.5.2/gni/mpich2-gnu/47)
+    /opt/cray/mpt/default/gni/mpich2-gnu/47)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
