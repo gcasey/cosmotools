0b64bd925084023f6fff7a410253ecc851e098e5
Robert Maynard <robert.maynard@kitware.com>
2013-04-26 10:09:24 -0400

    Refactor executables so that we can have tbb, cuda, and serial ones.
    For easier time benchmarking the performance improvements by
    using dax we need the ability to run tbb, cuda, and serial versions
    without having to recompile between each.

--- sfprobe/executables/cudaProbe.cu
+++ sfprobe/executables/cudaProbe.cu
@@ -1,10 +1,10 @@
-#include "probe.h"
-
 //all we have to do is define the adapter here and how we use boost threads
 #ifdef USEDAX
 #  define BOOST_SP_DISABLE_THREADS
 #  define DAX_DEVICE_ADAPTER DAX_DEVICE_ADAPTER_CUDA
 #endif
+
+#include "probe.h"
 
 int main(int argc, char **argv)
 {
--- sfprobe/executables/probe.cxx
+++ sfprobe/executables/probe.cxx
@@ -1,9 +1,10 @@
+
+//setup this as the serial only version
+#ifdef USEDAX
+# undef USEDAX
+#endif
 #include "probe.h"
 
-//all we have to do is define the adapter here
-#ifdef USEDAX
-#  define DAX_DEVICE_ADAPTER DAX_DEVICE_ADAPTER_TBB
-#endif
 
 int main(int argc, char **argv)
 {
