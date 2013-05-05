27fa22600ae15b34591d0e9d134c1baa9e9c6cc2
George Zagaris <george.zagaris@kitware.com>
2013-05-05 17:52:33 -0400

    ENH: Simple TaskTimer class for timing

---
+++ common/TaskTimer.h
@@ -0,0 +1,42 @@
+/**
+ * @brief A class that provides simple functionality for timing
+ */
+
+#ifndef TASKTIMER_H_
+#define TASKTIMER_H_
+
+#include "CosmologyToolsMacros.h"
+
+namespace cosmotk {
+
+class TaskTimer
+{
+public:
+	TaskTimer();
+	virtual ~TaskTimer();
+
+	/**
+	 * @brief Starts a timer.
+	 */
+	void StartTimer();
+
+	/**
+	 * @brief Stops the timer.
+	 */
+	void StopTimer();
+
+	/**
+	 * @return ellapsedTime the ellapsed time.
+	 */
+	double GetEllapsedTime();
+
+protected:
+	double StartTime;
+	double EndTime;
+
+private:
+	DISABLE_COPY_AND_ASSIGNMENT(TaskTimer);
+};
+
+} /* namespace cosmotk */
+#endif /* TASKTIMER_H_ */
