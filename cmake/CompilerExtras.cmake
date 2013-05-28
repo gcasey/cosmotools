5fc8607857a500932414716f3663c0d893560a2e
George Zagaris <george.zagaris@kitware.com>
2013-05-28 16:05:47 -0400

    COMP: Conform to C99 standard

--- cmake/CompilerExtras.cmake
+++ cmake/CompilerExtras.cmake
@@ -4,7 +4,7 @@ if(CMAKE_COMPILER_IS_GNUCC)
   option(ENABLE_WARNINGS "Enable Warnings" OFF)
   if(${ENABLE_WARNINGS})
     set(CXXWARNINGS "-Wnon-virtual-dtor -Wno-long-long -ansi -Wcast-align -Wchar-subscripts -Wall -Wextra -Wpointer-arith -Wformat-security -Woverloaded-virtual -Wshadow -Wunused-parameter -fno-check-new -fno-common")
-    set(CWARNINGS "-Wno-long-long -ansi -Wcast-align -Wchar-subscripts -Wall -Wextra -Wpointer-arith -Wformat-security -Wshadow -Wunused-parameter -fno-common")
+    set(CWARNINGS "-Wno-long-long -std=c99 -Wcast-align -Wchar-subscripts -Wall -Wextra -Wpointer-arith -Wformat-security -Wshadow -Wunused-parameter -fno-common")
   endif() # END if warnings are enabled
 endif() # END if compiler is GNU
 
